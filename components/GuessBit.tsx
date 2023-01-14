c963567e372bc59b53030b41e401b28531f8f8d5
Moritz Jansen <mojansen95@gmail.com>
2023-01-14 12:33:17 +0100

    Only show pointer if active row

--- components/GuessBit.tsx
+++ components/GuessBit.tsx
@@ -5,15 +5,16 @@ interface Props{
     colorId: number
     selected: boolean
     rowId: number
+    isSelectedRow: boolean
     updateSelectedRow: (row: number, column: number) => void
 }
 
     
 
-export default function GuessBit({id, colorId, selected, rowId, updateSelectedRow}: Props){
+export default function GuessBit({id, colorId, selected, rowId, isSelectedRow, updateSelectedRow}: Props){
     
     function clickHandler(){
         updateSelectedRow(rowId, id)
     }
-    return <div onClick={clickHandler} className={"w-1/4 aspect-square rounded-full transition-shadow " + colorStrings[colorId]  + (selected?' selected-cell':'')+ ((colorId === -1)? " guess-empty":" guess")}></div>
+    return <button onClick={clickHandler} className={"w-1/4 aspect-square rounded-full transition-shadow " + colorStrings[colorId]  + (selected?' selected-cell':'')+ ((colorId === -1)? " guess-empty":" guess") + (!isSelectedRow? " cursor-default":"")}></button>
 }
