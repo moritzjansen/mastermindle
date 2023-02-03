b6aa2d954089ba69a48b68019de9225b542b90c5
Moritz Jansen <mojansen95@gmail.com>
2023-02-03 13:25:33 +0100

    Rework Hints Layout

--- components/GameRowHints.tsx
+++ components/GameRowHints.tsx
@@ -7,7 +7,7 @@ interface Props{
 
 export default function GameRowHints({hint}:Props){
     return (
-        <div className="grid-cols-2 w-1/5 ml-[5%] mr-[3%] aspect-square scale-[calc(1/1.4142)] relative border border-slate-400 grid gap-2 p-[6px] sm:p-[10px] rotate-45">
+        <div className="flex flex-row flex-wrap justify-around items-center w-1/5 ml-[5%] mr-[3%] aspect-square scale-[calc(1/1.4142)] border border-slate-400 rotate-45 p-1">
                 <HintDot key={"hint-1"} status={hint[0]}></HintDot>
                 <HintDot key={"hint-2"} status={hint[2]}></HintDot>
                 <HintDot key={"hint-3"} status={hint[1]}></HintDot>
--- components/HintDot.tsx
+++ components/HintDot.tsx
@@ -5,5 +5,5 @@ interface Props{
 }
 
 export default function HintDot({status}:Props){
-    return <div className={"hint aspect-square rounded-full border border-slate-400 " + status}></div>
+    return <div className={"hint w-[35%] m-[6%] aspect-square rounded-full border border-slate-400 " + status}></div>
 }
